dc1ee4a3b3c7f76a506a7a6ed7403307ac733ed5
junster09 <ajr2z2025@gmail.com>
2026-03-31 13:36:39 -0700

    moved root

--- Scripts/Data Scripts/UID_Database.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-const PANELS = {
-    "SKILL_PANEL" : "uid://dih7du0qvwh6j",
-    "SKILL_CREATE_PANEL" : "uid://chd2gxpekd2kp"
-}
-
--- Scripts/Node Scripts/Create_Skill_Panel.gd
+++
@@ -1,6 +0,0 @@
-extends Panel
-
-@export var nameInput:LineEdit
-@export var rankField:MenuButton
-@export var levelField:LineEdit
-@export var usedField:LineEdit
